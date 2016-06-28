5e4ad3e49277c385243fc30dde7703673a2133a2
Fariz Izwan <Fariz@ahmads-macbook-pro.local>
2016-06-28 21:59:10 +0800

    new hello world

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,16 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    weak var greetingLabel: UILabel!
+    weak var greetButton: UIButton!
+    weak var byeButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.redColor()
+        
+        let label: UILabel = UILabel(frame: CGRect(x: 80, y: 40, width: 160, height: 100))
+        label.text = "Choose Animal"
+        label.textColor = UIColor.whiteColor()
+        label.backgroundColor = UIColor.brownColor()
+        self.view.addSubview(label)
+        self.greetingLabel = label
+        
+        let greetButton: UIButton = UIButton(frame: CGRect(x: 16, y: 300, width: 80, height: 50))
+        greetButton.backgroundColor = UIColor.blackColor()
+        greetButton.setTitle("Kucing",forState: UIControlState.Normal)
+        greetButton.addTarget(self, action: "buton:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(greetButton)
+        self.greetButton = greetButton
+        
+        let byeButton: UIButton = UIButton(frame: CGRect(x: 300, y: 300, width: 80, height: 50))
+        byeButton.backgroundColor = UIColor.blackColor()
+        byeButton.setTitle("Doggi",forState: UIControlState.Normal)
+        byeButton.addTarget(self, action: "buton:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(byeButton)
+        self.greetButton = byeButton
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func buton(sender: AnyObject){
+        if (sender as! UIButton) == self.greetButton{
+            self.greetingLabel.text = "🐶"
+        }else{
+            self.greetingLabel.text = "🐱"
+        }
+    }
+    
 
 
 }
